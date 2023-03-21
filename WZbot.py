0d4bf12f4489a27aaed51e6a74a9c1c7d21aeb5e
Alexander <116796202+alexanderkly@users.noreply.github.com>
2023-03-21 13:22:33 +0530

    Update WZbot.py

--- WZbot.py
+++ WZbot.py
@@ -4,8 +4,8 @@ from parsingworkzilla import parsing
 from aiogram.dispatcher.filters import Text
 import asyncio
 
-token = '6050222744:AAGXHZFi1igkAa3jY-gUdzCR83FmofOiwN4'
-user_id = '1581750481'
+token = 'TOKEN'
+user_id = 'USER_ID'
 
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
